113d9a4a02fd2302c470cc359471a22c5a5452cf
ldavu <41762352+ldavu@users.noreply.github.com>
2021-08-20 12:42:16 -0300

    Update 1-harrypoter.py

--- 1-harrypoter.py
+++ 1-harrypoter.py
@@ -8,6 +8,6 @@
 
 encantamento_portal_pergunta = input (str("Qual é o encantamento para abrir o portal?\n"))
 
-if encantamento_portal_pergunta == "Destravium Laparada" or "DESTRAVIUM LAPARADA" or "destravium laparada":
+if encantamento_portal_pergunta == "Destravium Laparada":
     print(f"O portal foi aberto após dizer!{encantamento_portal_pergunta}")
 
